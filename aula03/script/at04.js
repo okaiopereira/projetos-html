0f17f2d254cf37b01fa2f106ee071048ac70bf7d
Kaio <kaiopereira5000@gmail.com>
2025-10-05 18:04:52 -0300

    Delete aula03 directory

--- aula03/script/at04.js
+++
@@ -1,9 +0,0 @@
-const DetalhesElemento = document.querySelector('details');
-const abrir = document.querySelector('summary');
-const resp = abrir.nextElementSibling;
-
-abrir.addEventListener('click', function(event){
-    
-    resp.classList.toggle('escondido');
-    DetalhesElemento.classList.toggle('aberto-js')
-})
